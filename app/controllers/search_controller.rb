fe01c537c008cbdf673719ec36e8909c5ff1fbf7
Ademosu Ayodele <ayoquirk@yahoo.com>
2013-03-15 10:18:22 +0000

    Final Work Done

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,7 @@ class SearchController < ApplicationController
 	include ApplicationHelper
 	include SessionsHelper
 	def index
-      search = "%#{:search.upcase}%"
-		@libraries = Library.query(params[search])
+		@libraries = Library.query(params[:search])
   	end
 
 	def show
--- app/models/library.rb
+++ app/models/library.rb
@@ -2,6 +2,6 @@ class Library < ActiveRecord::Base
   attr_accessible :name, :address, :postcode, :dialling, :telephone, :email, :access_details
 	def self.query(search)
 	    search_condition =  "%#{search}%"
-    	find(:all, :conditions => ['postcode LIKE ? OR address LIKE ? ', search_condition, search_condition])
+    	find(:all, :conditions => ['postcode LIKE ?', search_condition])
 	end
 end
